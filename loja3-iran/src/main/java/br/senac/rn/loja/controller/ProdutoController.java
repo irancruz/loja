f8ccbeb23d127abb449c185dd8bf5ef04875a621
IRANILSON SOARES DA CRUZ <a93559@rn.senac.br>
2018-10-09 20:45:50 -0300

    projeto iran

---
+++ loja3-iran/src/main/java/br/senac/rn/loja/controller/ProdutoController.java
@@ -0,0 +1,52 @@
+package br.senac.rn.loja.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.senac.rn.loja.model.Departamento;
+import br.senac.rn.loja.model.Marca;
+import br.senac.rn.loja.model.Produto;
+import br.senac.rn.loja.service.DepartamentoService;
+import br.senac.rn.loja.service.MarcaService;
+import br.senac.rn.loja.service.ProdutoService;
+
+@Controller
+@RequestMapping("produto")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoService service;
+	@Autowired
+	private MarcaService marcaService;
+	@Autowired DepartamentoService departamentoService;
+	
+	@GetMapping
+	public String lista(Model model) {
+		List<Produto> produtos = service.obterTodos();
+		model.addAttribute("produtos", produtos);
+		return "produto/lista";
+	}
+	
+	@GetMapping("cadastrar")
+	public String novo (Model model) {
+		Produto produto = new Produto();
+		model.addAttribute("produto", produto);
+		List<Marca> marcas = marcaService.obterTodos();
+		List<Departamento> departamentos = departamentoService.buscarTodos();
+		model.addAttribute("marcas", marcas);
+		model.addAttribute("departamentos", departamentos);		
+		return "produto/form";
+	}
+	
+	@PostMapping
+	public String salvar(Produto produto) {
+		service.salvar(produto);		
+		return "redirect/produto";
+	}
+}
